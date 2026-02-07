782c1b12125cfe57aefb82986c69dd852c8df6d4
Marcus Behel <marcus.behel@mindspring.com>
2026-02-07 11:34:36 -0500

    Implement copy to destination

--- backup.sh
+++ backup.sh
@@ -6,7 +6,6 @@
 ####################################################################################################
 # Setup
 ####################################################################################################
-
 # Only allow this script to run interactive
 if ! [ -t 0 ]; then
     echo "Must run interactive" >&2
@@ -15,7 +14,6 @@ fi
 
 # Run in same directory as this script
 cd "$(dirname "$(realpath "$0")")"
-
 ####################################################################################################
 
 
@@ -55,7 +53,6 @@ if [ $IS_MAC -eq 1 ]; then
     echo "MacOS is not currently supported" >&2
     exit 1
 fi
-
 ####################################################################################################
 
 
@@ -70,6 +67,7 @@ DEST_SHARE=""           # SMB share name
 DEST_PATH=""            # SMB path
 DEST_USER=""            # SMB username
 DEST_PASS=""            # SMB password
+DEST_TYPE=""            # normal or smb
 mkdir -p "$CONFIG_DIR"
 [ -f "$CONFIG_DIR/settings.sh" ] && . "$CONFIG_DIR/settings.sh"
 
@@ -90,12 +88,14 @@ while true; do
         "")
             ;;
         "smb://"*)
+            DEST_TYPE="smb"
             DEST_NOPREFIX="$(echo "$DEST" | sed 's#smb://##g')"
             DEST_SERVER="$(echo "$DEST_NOPREFIX" | cut -d/ -f1)"
             DEST_SHARE="$(echo "$DEST_NOPREFIX" | cut -d/ -f2)"
             DEST_PATH="$(echo "$DEST_NOPREFIX" | sed "s#$DEST_SERVER/$DEST_SHARE/##g")"
             read -p "SMB User: " DEST_USER
             read -p "SMB Pass: " -s DEST_PASS
+            echo ""
             if smbc "cd \"$DEST_PATH\"" >/dev/null 2>&1; then
                 TEST_FILE="$(mktemp)"
                 echo "write test" > "$TEST_FILE"
@@ -110,6 +110,7 @@ while true; do
             ;;
         *)
             # Make sure destination exists and is writable
+            DEST_TYPE="normal"
             if [ -d "$DEST" ]; then
                 if echo "write test" > "$DEST/test_$(hostname)" 2>&1; then
                     break
@@ -121,10 +122,83 @@ while true; do
             fi
             ;;
     esac
-    DEST=""
 done
 
 # Save settings
 cat << EOF > "$CONFIG_DIR/settings.sh"
 DEST="$DEST"
 EOF
+####################################################################################################
+
+
+
+####################################################################################################
+# Create backup tar
+####################################################################################################
+
+# Setup temp working directory where we will copy saves to create tar
+WORKDIR="$(mktemp -d)"
+trap "rm -rf \"$WORKDIR\"" EXIT
+DATESTRING="$(date '+%Y%m%d_%I%M%p')"
+BACKUP_NAME="$(hostname)_$DATESTRING"
+mkdir "$WORKDIR/$BACKUP_NAME"
+
+# TODO: Make real backup
+echo "Test A" > "$WORKDIR/$BACKUP_NAME/a.txt"
+echo "Test B" > "$WORKDIR/$BACKUP_NAME/b.txt"
+mkdir "$WORKDIR/$BACKUP_NAME/c"
+echo "Test C" > "$WORKDIR/$BACKUP_NAME/c/c.txt"
+
+# Choose compression method (busybox for windows has xz, but it can only decompress)
+COMPRESS=".gz"
+oldcwd="$(pwd)"
+cd "$WORKDIR"
+echo "test" > test.txt
+if xz -z -T0 test.txt >/dev/null 2>&1; then
+    COMPRESS=".xz"
+fi
+cd "$oldcwd"
+
+# Make tar & compress
+oldcwd="$(pwd)"
+cd "$WORKDIR"
+tar -cf "$BACKUP_NAME.tar" "$BACKUP_NAME/"
+case "$COMPRESS" in
+    ".xz")
+        xz -z -T0 "$BACKUP_NAME.tar"
+        ;;
+    ".gz")
+        gzip "$BACKUP_NAME.tar"
+        ;;
+    *)
+        echo "Unknown compression method attempted. Skipping"
+        COMPRESS=""
+        ;;
+esac
+cd "$oldcwd"
+
+# Move backup to destination
+case "$DEST_TYPE" in
+    normal)
+        if ! cp "$WORKDIR/$BACKUP_NAME.tar$COMPRESS" "$DEST/"; then
+            echo "Failed to save backup"
+            exit 1
+        fi
+        ;;
+    smb)
+        if ! smbc "cd \"$DEST_PATH\"; put \"$WORKDIR/$BACKUP_NAME.tar$COMPRESS\" \"$BACKUP_NAME.tar$COMPRESS\""; then
+            echo "Failed to save backup"
+            exit 1
+        fi
+        ;;
+    *)
+        echo "Uknonwn destination type. This is a script bug!!!"
+        echo "No backup has been saved!!!"
+        exit 1
+        ;;
+esac
+echo "Backup complete!"
+# TODO: Pause before exit
+
+####################################################################################################
+
