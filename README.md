ab27cc5c01d869ca55b448f97a06f6b358089a5a
Marcus Behel <marcus.behel@mindspring.com>
2026-02-07 15:32:55 -0500

    Add license

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # GameBackupScripts
 
-Scripts to backup game saves
+Scripts to backup game saves on Linux and Windows (msys2 or busybox required on windows)
+
+Supports backup to local paths or smb shares on Linux systems
+
+Usage: `./backup.sh` (must run interactive)
 
--- backup.sh
+++ backup.sh
@@ -1,6 +1,38 @@
 #!/usr/bin/env sh
-# TODO: HEader comment
-
+####################################################################################################
+#
+# Copyright 2026 Marcus Behel
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted 
+# provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions 
+# and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of 
+# conditions and the following disclaimer in the documentation and/or other materials provided with 
+# the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to 
+# endorse or promote products derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR 
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+####################################################################################################
+# script:      backup.sh
+# description: Backup game saves to a local or smb share location. Game detection is handled via
+#              additoional scripts for each game located in the games/ subdirectory. smb share
+#              backup is only supported on Linux systems with the smbclient command.
+#              Running on windows requires either msys2 or busybox. While compatability with busybox
+#              is intended, it is not tested often and may have issues.
+####################################################################################################
 
 
 ####################################################################################################
